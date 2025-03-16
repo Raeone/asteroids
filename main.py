dd5a02762fc66b6768e0ec8e872966fc0e42f5b4
Raeone <pay124@seznam.cz>
2025-03-16 19:17:30 +0100

    group players

--- main.py
+++ main.py
@@ -6,7 +6,10 @@ def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
-    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2 )
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2 )    
     dt = 0
 
     while True:
@@ -16,8 +19,9 @@ def main():
         
         screen.fill((0, 0, 0))
         
-        player.draw(screen)
-        player.update(dt)
+        for obj in drawable:
+            obj.draw(screen)
+        updatable.update(dt)
         
         pygame.display.flip() 
         dt = clock.tick(60) / 1000
