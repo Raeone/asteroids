8e2f9e7d15f5d59884cbe8077dcb4a42b3872cb4
Raeone <pay124@seznam.cz>
2025-03-16 18:52:26 +0100

    add player rotation

--- main.py
+++ main.py
@@ -15,9 +15,11 @@ def main():
                 return
         
         screen.fill((0, 0, 0))
+        
         player.draw(screen)
+        player.update(dt)
+        
         pygame.display.flip() 
-
         dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
