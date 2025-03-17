a9037e3b0beb8ac04c078e0031e5cac4638438c8
Raeone <pay124@seznam.cz>
2025-03-17 11:56:35 +0100

    add asteroids and their spawning and movement

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import pygame
 from constants import *
 from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 
 def main():
     # pygame initialization
@@ -15,10 +17,14 @@ def main():
     # group objects - create pygame groups
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
     # add containers attribut to Player class (to all player objects)
     Player.containers = (updatable, drawable)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = updatable
     # create player object
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2 )    
+    asteroid_field = AsteroidField()
 
     # delta time variable
     dt = 0
